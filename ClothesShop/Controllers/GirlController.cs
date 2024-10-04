3488e6d1329553c463a5a0bdb11a663b3f1e507f
cuongnquang <cuongnquang@gmail.com>
2024-10-04 13:34:45 +0700

    first commit

---
+++ ClothesShop/Controllers/GirlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesShop.Controllers
+{
+    public class GirlController : Controller
+    {
+        // GET: Girl
+        public ActionResult Girl()
+        {
+            return View();
+        }
+    }
+}
